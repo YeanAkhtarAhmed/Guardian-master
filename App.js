728535a37405b9e3662da342c1bdda894781316d
Yean Akhtar Ahmed <yean.ncrts@gmail.com>
2020-10-14 21:17:23 +0530

    commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, {Component,useEffect} from 'react';
+
+import {Text, View,Image,ImageBackground,TouchableOpacity,TextInput,FlatList,Platform} from 'react-native';
+import {responsiveWidth, responsiveFontSize,responsiveHeight } from 'react-native-responsive-dimensions';
+const backgroundIg = require('./assets/splash.png');
+import App from './src/App.js';
+import SplashScreen from 'react-native-splash-screen'
+
+
+export default class RootApp extends Component { 
+
+  render() {
+    return (
+    <App/>
+    );
+  }
+}
